c4039825f119b9d3d387665697b23d1770869632
vickrammark <vickrammark54@gmail.com>
2020-11-14 21:39:09 -0800

    New Base Entity is added

---
+++ Pet-Clinic/Pet-Clinic-Data/src/main/java/com/Project/PetClinic/PetClinic/Model/BaseEntity.java
@@ -0,0 +1,15 @@
+package com.Project.PetClinic.PetClinic.Model;
+
+public class BaseEntity {
+	
+	private Long id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+}
--- Pet-Clinic/Pet-Clinic-Data/src/main/java/com/Project/PetClinic/PetClinic/Model/PetType.java
+++ Pet-Clinic/Pet-Clinic-Data/src/main/java/com/Project/PetClinic/PetClinic/Model/PetType.java
@@ -1,6 +1,6 @@
 package com.Project.PetClinic.PetClinic.Model;
 
-public class PetType {
+public class PetType  extends BaseEntity{
 	
 	private String name;
 
